105fb37c633b02bd7f553cfe834c66d197a52dfc
hortonvilleroboticsgrace <41523342+hortonvilleroboticsgrace@users.noreply.github.com>
2019-03-26 16:44:59 -0500

    Fixed that little bugger there... here ya go

--- app/src/main/java/com/example/heuve/game/HomeScreen.java
+++ app/src/main/java/com/example/heuve/game/HomeScreen.java
@@ -4,20 +4,17 @@ import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class HomeScreen extends AppCompatActivity {
-
-    private TextView mTextMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
-
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-
     }
+
+    public static Button clickMe;
 
 }
