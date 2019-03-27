4ea6b05787ae80dc29ce96682b63020d2bcc2078
hortonvilleroboticsgrace <41523342+hortonvilleroboticsgrace@users.noreply.github.com>
2019-03-26 20:39:51 -0500

    So I made a main activity which will control the switching between fragments. Also I made a rough home screen layout idea.

---
+++ app/src/main/java/com/example/heuve/game/GameList.java
@@ -0,0 +1,9 @@
+package com.example.heuve.game;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class GameList extends AppCompatActivity{
+
+
+
+}
--- app/src/main/java/com/example/heuve/game/MainActivity.java
+++ app/src/main/java/com/example/heuve/game/MainActivity.java
@@ -6,30 +6,24 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class HomeScreen extends AppCompatActivity {
-
-    public static Button play;
-    public static TextView TITLE, playSomeGames;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
-        play = (Button)findViewById(R.id.play);
-        TITLE = (TextView)findViewById(R.id.Title);
-        playSomeGames = (TextView)findViewById(R.id.PlaySomeGames);
 
         View.OnClickListener c = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                setContentView(R.layout.game_list);
+                //figure something out to go up and down logical chain (home -> game choice -> game)
             }
         };
 
     }
 
     //I have it set up right now to basically do this
-    //      HomeScreen (Click play) --> GameList (Choose a Game from here) --> Play the Game
+    //      HomeScreen (Click play) -->  GameList (Choose a Game from here) -->  Play the Game
 
     //Also, how the heck are we going to get games into here? Reprogram them? Ima do some digging
 
